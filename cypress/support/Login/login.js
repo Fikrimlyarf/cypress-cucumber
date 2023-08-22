ba9d55262152942563949aaed5512cb064e6000e
Fikrimlyarf <fikrimlyarf@gmail.com>
2023-08-22 11:38:11 +0700

    update

--- cypress/support/Login/login.js
+++ cypress/support/Login/login.js
@@ -3,7 +3,7 @@ Cypress.Commands.add("loginsuperadmin", (idrole) => {
   cy.fixture("login/login_user").then((data) => {
     data.list.forEach((user)=>{
       if(idrole === user.id){
-        cy.get("#userid").type(user.username);
+        cy.get("#email").type(user.username);
         cy.get("#password").type(user.password);
       }
     })
