227431a5a5a8f77e0e97219a40258e4e2fe0c88d
Ardi <prastowoardi616@gmail.com>
2023-09-08 15:58:49 +0700

    hapus element tidak dipakai

--- cypress/e2e/rpl/periodePendaftaran.js
+++ cypress/e2e/rpl/periodePendaftaran.js
@@ -1,43 +1,6 @@
 import { When } from "@badeball/cypress-cucumber-preprocessor";
 import data from "../../fixtures/rpl/periodePendaftaran.json"
 
-// Menampung element field input text
-const inputText = {
-
-    // Element program studi
-    "kuota": "#jmlditerima",
-    "prefix nim": "#prefixnim",
-}
-
-const select2 = {
-    // Element program studi
-    "program studi": "#select2-idunit-container",
-    "admin prodi": "#idadminprodi",
-    "urut nim": "#select2-jmlurutannim-container",
-    "dosen asesor 1": "#select2-asesor_0-container",
-    "dosen asesor 2": "#select2-asesor_1-container",
-    "dosen asesor 3": "#select2-asesor_2-container"
-}
-
-// Menampung element sub menu
-const subMenu = {
-    // Element sub menu
-    "jenis program" : "#sidebar-menu-list > :nth-child(2) > a:contains('Jenis Program')",
-    "program studi" : "#sidebar-menu-list > :nth-child(3) > a:contains('Program Studi')",
-    "seleksi pendaftaran" : "#sidebar-menu-list > :nth-child(4) > a:contains('Seleksi Pendaftaran')",
-    "syarat pendaftaran" : "#sidebar-menu-list > :nth-child(6) > a:contains('Syarat Pendaftaran')",
-
-    // Element program studi
-    "tim asesor": "#item-assesor-rpl > a"
-}
-
-const checkBox = {
-    // Element program studi
-    "transfer kredit" : ":nth-child(3) > .icheckbox_minimal > .iCheck-helper",
-    "perolehan kredit" : ":nth-child(5) > .icheckbox_minimal > .iCheck-helper"
-}
-
-
 When ("User menambahkan {string}", (menu) => {
     if (menu == "periode pendaftaran") {
         cy.get('.btn').contains("Tambah").click()
