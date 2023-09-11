123ca9cbbfc94f1306fd1f8c653897d13d8ea7c4
Ardi <prastowoardi616@gmail.com>
2023-09-11 16:22:50 +0700

    tambah prodi di periode pendaftaran

--- cypress/e2e/rpl/periodePendaftaran.js
+++ cypress/e2e/rpl/periodePendaftaran.js
@@ -1,11 +1,12 @@
 import { When } from "@badeball/cypress-cucumber-preprocessor";
-import data from "../../fixtures/rpl/periodePendaftaran.json"
+import dataPeriod from "../../fixtures/rpl/periodePendaftaran.json"
+import dataProdi from "../../fixtures/rpl/prodi.json"
 
-When ("User menambahkan {string}", (menu) => {
+When ("User menambahkan {string} dengan jalur RPL", (menu) => {
     if (menu == "periode pendaftaran") {
         cy.get('.btn').contains("Tambah").click()
         //input data secara looping
-        data.dataPeriode.forEach((periode)=>{
+        dataPeriod.dataPeriode.forEach((periode)=>{
             cy.get('#namaperiodedaftar').type(periode.nama)
             cy.get('#select2-idgelombang-container').type(periode.gelombang + '{enter}')
             cy.get('#select2-idjalurpendaftaran-container').type(periode.jalur + '{enter}')
@@ -30,3 +31,59 @@ When ("User menambahkan {string}", (menu) => {
         })
     }
 })
+
+When("User menambahkan prodi dengan {string} asesor", (jumlahAsesor) => {
+    cy.get("input.form-control.input-sm").type('Rekognisi Pembelajaran Lampau{enter}')
+    cy.get('.table').contains('Gelombang 5').parent().find('.btn-info').click()
+  
+    cy.get('#sidebar-menu-list > :nth-child(3) > a').contains('Program Studi').click()
+  
+    dataProdi.prodi.forEach((prodi) => {
+      cy.get('.btn-success').contains('Tambah').click()
+  
+      // Mengisi Form prodi dengan jalur RPL transfer kredit
+      cy.get('#select2-idunit-container').type(prodi.prodi + '{enter}')
+      cy.get('#idadminprodi')
+      cy.get('#jmlditerima').type(prodi.kuota)
+      cy.get('#prefixnim').type(prodi.prefixNim)
+      cy.get('#select2-jmlurutannim-container').type(prodi.urutNim + '{enter}')
+      cy.get(':nth-child(3) > .icheckbox_minimal > .iCheck-helper').click()
+  
+      // Memilih Asesor
+      cy.get('#item-assesor-rpl > a').click()
+      const total = parseInt(jumlahAsesor)
+  
+      // Cek apakah jumlah asesor >1, jika iya maka klik tombol tambah asesor
+      if (total > 1) {
+        const klik = total - 1; // Hitung berapa kali tombol tambah asesor harus diklik
+        for (let i = 0; i < klik; i++) {
+          cy.get('#tr_tambahasesor > :nth-child(1) > .row > .col-md-8 > .btn').click()
+        }
+      }
+  
+      dataProdi.asesor.forEach((asesor, index) => {
+        if (index < total && asesor.prodiAsal === prodi.prodi) {
+            cy.log(`Prodi Asesor: ${asesor.prodiAsal} ${prodi.prodi}`)
+          const asesorData = asesor
+          if (asesorData) {
+            const searchValue = asesorData.search
+            const suggestValue = asesorData.suggest
+  
+            cy.get(`#select2-asesor_${index}-container`).type(searchValue)
+            cy.get('.select2-results').each(($el) => {
+              if ($el.text() === suggestValue) {
+                cy.wrap($el).click()
+  
+                // Tambahkan log untuk mencatat bahwa asesor telah dipilih
+                cy.log(`Asesor untuk prodi '${prodi.prodi}' telah dipilih: ${searchValue}`)
+              }
+            })
+          }
+        } else {
+          console.error(`Asesor tidak tersedia pada data fixture`)
+        }
+      })
+      cy.get('.btn-success').contains('Simpan').click()
+      cy.get('#modal-konfirmasi > .modal-footer > .btn-primary').click()
+    })
+  })  
